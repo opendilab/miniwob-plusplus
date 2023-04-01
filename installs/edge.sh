dd282170c4385916c8117de1aacce7a31e511169
HansBug <hansbug@buaa.edu.cn>
2023-04-01 14:46:40 +0800

    dev(hansbug): fix bug for edge

--- installs/edge.sh
+++ installs/edge.sh
@@ -1,6 +1,6 @@
 # install edge browser
 pip install requests &&
-  STABLE_VERSION=$(python -c 'import requests;print(requests.get("https://huggingface.co/HansBug/browser_drivers_mirror/resolve/main/edge/LATEST_STABLE").content.decode("utf-16").strip());') &&
+  STABLE_VERSION=$(wget -qO- 'https://huggingface.co/HansBug/browser_drivers_mirror/resolve/main/edge/LATEST_STABLE') &&
   wget -q "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_${STABLE_VERSION}-1_amd64.deb" &&
   apt-get install -y "./microsoft-edge-stable_${STABLE_VERSION}-1_amd64.deb" &&
   rm -rf "./microsoft-edge-stable_${STABLE_VERSION}-1_amd64.deb" &&
